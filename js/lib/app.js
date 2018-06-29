b97ce753e6a20f84dbda6e088a02c9f63ad08d35
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2018-06-29 20:24:17 -0300

    Atualizando o base url da API

--- js/lib/app.js
+++ js/lib/app.js
@@ -1,8 +1,7 @@
 var App = {
 
-    // contextPath: "https://despesas-despesas.a3c1.starter-us-west-1.openshiftapps.com",
-    contextPath: "https://wildfly-despesas.a3c1.starter-us-west-1.openshiftapps.com",
-    //contextPath: "http://localhost:8080",
+    contextPath: "https://wildfly-mensal.a3c1.starter-us-west-1.openshiftapps.com",
+    // contextPath: "http://localhost:8080",
 
     getParam: function (name) {
         return new URL(document.location).searchParams.get(name)
