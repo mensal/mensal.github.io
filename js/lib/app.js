12d8b581f5696a1fdd4158ad78c9287891c67929
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2018-09-04 23:01:31 -0300

    Testando o servidor local

--- js/lib/app.js
+++ js/lib/app.js
@@ -1,6 +1,7 @@
 var App = {
 
-    contextPath: "https://wildfly-mensal.a3c1.starter-us-west-1.openshiftapps.com",
+    contextPathh: "mensal.ddns.net:8080",
+    // contextPath: "https://wildfly-mensal.a3c1.starter-us-west-1.openshiftapps.com",
     // contextPath: "http://localhost:8080",
 
     getParam: function (name) {
